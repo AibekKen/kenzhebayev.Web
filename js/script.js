2160861936e03b0332243fd5b0803c2ae3bb8fda
Aibek <ai.bek@mail.ru>
2022-03-15 19:55:29 +0600

    js code for animation on scroll

--- js/script.js
+++ js/script.js
@@ -1,27 +1,3 @@
-const burger = document.querySelector('.header__burger');
-const menu = document.querySelector('.header__menu');
-const menuLink = document.querySelectorAll('.menu__link');
-const block = document.createElement('div');
-const logo = document.querySelector('.header__logo');
-document.body.append(block);
-
-burger.addEventListener('click', () => {
-   burger.classList.toggle('active');
-   menu.classList.toggle('active');
-   document.body.classList.toggle('lock');
-   block.classList.toggle('block');
-   logo.classList.toggle('inactive')
-})
-
-menuLink.forEach((link, index) => link.addEventListener('click', () => {
-   burger.classList.remove('active');
-   menu.classList.remove('active');
-   document.body.classList.remove('lock');
-   block.classList.remove('block');
-   logo.classList.remove('inactive');
-
-}))
-
 
 function ibg() {
 
@@ -35,6 +11,44 @@ function ibg() {
 
 ibg();
 
+const aboutProductBody = document.querySelector('.about-product__body');
+const labels = document.querySelectorAll('.label');
+
+function show_onscroll() {
+   let wt = window.scrollY;
+   let wh = document.body.clientHeight;
+   let eh = aboutProductBody.offsetHeight;
+   let et = aboutProductBody.offsetTop;
+   if (wt + wh > et + eh * 0.4)
+      return true;
+}
+
+window.addEventListener('scroll', function (e) {
+   if (show_onscroll()) {
+      aboutProductBody.classList.add("active");
+      labels.forEach((label) => label.classList.add("active"));
+   }
+});
+
+const burger = document.querySelector('.header__burger');
+const menu = document.querySelector('.header__menu');
+const menuLink = document.querySelectorAll('.menu__link');
+const block = document.createElement('div');
+document.body.append(block);
+
+burger.addEventListener('click', () => {
+   burger.classList.toggle('active');
+   menu.classList.toggle('active');
+   document.body.classList.toggle('lock');
+   block.classList.toggle('block');
+})
+
+menuLink.forEach((link, index) => link.addEventListener('click', () => {
+   burger.classList.remove('active');
+   menu.classList.remove('active');
+   document.body.classList.remove('lock');
+   block.classList.remove('block');
+}))
 
 
 
